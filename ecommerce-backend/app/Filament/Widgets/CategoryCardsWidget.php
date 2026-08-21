d802e07ff5a725b8a5b3af93413dfdab7f695b6a
cyber-proximity <cyberproximity@gmail.com>
2026-08-21 16:29:05 +0100

    Revert "Fix CategoryCardsWidget: use non-static $view for Filament v4 compatibility"
    This reverts commit 1ba5a50c803657ffbd78c1796e9d39b56f200323.

--- ecommerce-backend/app/Filament/Widgets/CategoryCardsWidget.php
+++ ecommerce-backend/app/Filament/Widgets/CategoryCardsWidget.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 
 class CategoryCardsWidget extends Widget
 {
-    protected string $view = 'filament.widgets.category-cards-widget';
+    protected static string $view = 'filament.widgets.category-cards-widget';
 
     protected int|string|array $columnSpan = 'full';
 
